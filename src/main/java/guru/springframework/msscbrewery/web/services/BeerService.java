421447a35b84438f9f3c9d6551fa5edbfd1e6804
Luis Kupferberg <lkupferberg@overactive.com>
2020-07-04 02:02:13 -0400

    -- Removing unnecessary files. -- Adding the rest controller, the service.

---
+++ src/main/java/guru/springframework/msscbrewery/web/services/BeerService.java
@@ -0,0 +1,13 @@
+package guru.springframework.msscbrewery.web.services;
+
+import guru.springframework.msscbrewery.web.model.BeerDto;
+
+import java.util.UUID;
+
+/**
+ * @author Luis M. Kupferberg Ruiz (lkupferberg@overactive.com)
+ * @created 2020-07-04 01:25
+ */
+public interface BeerService {
+    BeerDto getBeerById(UUID beerId);
+}
---
+++ src/main/java/guru/springframework/msscbrewery/web/services/BeerServiceImpl.java
@@ -0,0 +1,22 @@
+package guru.springframework.msscbrewery.web.services;
+
+import guru.springframework.msscbrewery.web.model.BeerDto;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+/**
+ * @author Luis M. Kupferberg Ruiz (lkupferberg@overactive.com)
+ * @created 2020-07-04 01:26
+ */
+@Service
+public class BeerServiceImpl implements BeerService {
+  @Override
+  public BeerDto getBeerById(UUID beerId) {
+    return BeerDto.builder()
+        .id(UUID.randomUUID())
+        .beerName("Galaxy Cat")
+        .beerStyle("Pale Ale")
+        .build();
+  }
+}
