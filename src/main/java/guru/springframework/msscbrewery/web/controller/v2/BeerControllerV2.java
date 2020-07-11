59c64a6bab4745ef34c19cbcdafba532da19ae66
Luis Kupferberg <lkupferberg@overactive.com>
2020-07-10 21:24:01 -0400

    Added Exception handling.

--- src/main/java/guru/springframework/msscbrewery/web/controller/v2/BeerControllerV2.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/v2/BeerControllerV2.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -38,7 +43,7 @@ public class BeerControllerV2 {
   }
 
   @PostMapping
-  public ResponseEntity create(@RequestBody BeerDtoV2 beerDtoV2) {
+  public ResponseEntity create(@Valid @RequestBody BeerDtoV2 beerDtoV2) {
     BeerDtoV2 createdBeer = beerServiceV2.create(beerDtoV2);
     HttpHeaders httpHeaders = new HttpHeaders();
     // todo add the hostname url
@@ -49,7 +54,7 @@ public class BeerControllerV2 {
 
   @PutMapping({"/{beerId}"})
   public ResponseEntity update(
-      @PathVariable("beerId") UUID beerId, @RequestBody BeerDtoV2 beerDtoV2) {
+      @PathVariable("beerId") UUID beerId, @Valid @RequestBody BeerDtoV2 beerDtoV2) {
     beerServiceV2.update(beerId, beerDtoV2);
 
     return new ResponseEntity(HttpStatus.NO_CONTENT);
@@ -59,5 +64,19 @@ public class BeerControllerV2 {
   @ResponseStatus(HttpStatus.NO_CONTENT)
   public void deleteById(@PathVariable("beerId") UUID beerId) {
     beerServiceV2.deleteById(beerId);
+  }
+
+  @ExceptionHandler(ConstraintViolationException.class)
+  public ResponseEntity<List> validationErrorHandler(ConstraintViolationException e) {
+    List<String> errors = new ArrayList<>(e.getConstraintViolations().size());
+
+    e.getConstraintViolations()
+        .forEach(
+            constraintViolation -> {
+              errors.add(
+                  constraintViolation.getPropertyPath() + ": " + constraintViolation.getMessage());
+            });
+
+    return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
   }
 }
