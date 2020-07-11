b89378cb6ca8b9e6832e535aaadadc41e613e52b
Luis Kupferberg <lkupferberg@overactive.com>
2020-07-11 19:06:51 -0400

    -- Added mapstruct example.

---
+++ src/main/java/guru/springframework/msscbrewery/domain/Beer.java
@@ -0,0 +1,24 @@
+package guru.springframework.msscbrewery.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+/**
+ * @author Luis M. Kupferberg Ruiz (lkupferberg@overactive.com)
+ * @created 2020-07-11 18:58
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Beer {
+
+    private UUID id;
+    private String beerName;
+    private String beerStyle;
+    private Long upc;
+}
